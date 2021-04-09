4edc1f9454295fe60c648c5ef7a635aaec882154
luke <lukedesond@hotmail.co.uk>
2021-04-09 17:47:21 +0100

    fixed bs

---
+++ code-quiz.js
@@ -0,0 +1,16 @@
+document.getElementById("StartButton").addEventListener("click", function(){
+    var timeleft = 15;
+
+    var downloadTimer = setInterval(function function1(){
+    document.getElementById("countdown").innerHTML = timeleft + 
+    "&nbsp"+"seconds remaining";
+
+    timeleft -= 1;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Time is up!"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
